d613d17946405060231575c0e425af2efc56b9f2
Amos Brazzoli <amos.brazzoli@gmail.com>
2020-02-26 15:40:27 +0100

    Linear DQN

--- model.py
+++ model.py
@@ -3,10 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DQN(nn.Module):
-    def __init__(self, batch_size, channels, width, height):
+    def __init__(self, length):
         super(self, DQN).__init__()
 
+        lin1 = nn.Linear(length, length//3)
+        lin2 = nn.Linear(length//3, length//2)
+        lin3 = nn.Linear(length//2, length)
 
-        
-
-
+    def forward(self, x):
+        x = F.relu(self.lin1(x))
+        x = F.relu(self.lin2(x))
+        return F.relu(self.lin3(x))
